612c634087b87ded568b57c32a92f0412c7a286c
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-07-17 17:36:24 -0600

    home page css in progress

--- SKO-frontend/sko-frontend/src/components/Home/Home.js
+++ SKO-frontend/sko-frontend/src/components/Home/Home.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import "./Home.css"
 
 export class Home extends Component {
     render() {
         return (
             <div>
-                
+                <div className='input-trending'>
+
+                    <div className='input'>
+                        <form className='input'>
+                            <input 
+                            placeholder='Search bar'
+                            />
+                            <button>Enter</button>
+                            </form>
+                        
+                    </div>
+                    <div className='trending'>
+                        Trending
+                    </div>
+                </div>
             </div>
         )
     }
