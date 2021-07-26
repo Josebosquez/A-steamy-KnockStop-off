5872aaac93fd88ee56e7e9462b45e4ccb1bc5d45
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-07-26 13:10:29 -0600

    added sign up verification for fname, lname, email, and password

--- SKO-frontend/sko-frontend/src/components/Signup/Signup.js
+++ SKO-frontend/sko-frontend/src/components/Signup/Signup.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import checkIfAuth from '../../utils/checkIfAuth'
 import { Link } from 'react-router-dom'
-
-// import { isAlpha, isEmail, isAlphanumeric, isStrongPassword } from "validator"
+import { isAlpha, isEmail, isAlphanumeric, isStrongPassword } from "validator"
 
 import "./Signup.css"
 
@@ -13,23 +13,135 @@ export class Signup extends Component {
         email: "",
         password: "",
         confirmPassword: "",
+
+        firstNameError: "", 
+        lastNameError: "",
+        usernameError: "",
+        emailError: "",
+        passwordError: "",
+
+        isButtonDisabled: true,
     }
+componentDidMount(){
+    let isAuth = checkIfAuth();
+    if (isAuth){
+        this.props.history.push()
+    }
+}
 
     handleOnChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
-        })
-        console.log(event.target.value)
-    }
+        },() => {
+            if (
+                event.target.name === "firstName" ||
+                event.target.name === "lastName"
+            ) {
+                this.handleFirstNameAndLastNameInput(event);
+            }
+
+            if (event.target.name === "email") {
+                this.handleEmailInput();
+            }
+
+            if (event.target.name === "username") {
+                this.handleUsernameInput();
+            }
+            if (event.target.name === "password") {
+                this.handlePasswordInput();
+            }
+
+            if (event.target.name === "confirmPassword") {
+                this.handleConfirmPasswordInput();
+            }
+        }
+    );
+    };
+
+    handleFirstNameAndLastNameInput = (event) => {
+        if (this.state[event.target.name].length > 0) {
+            if (isAlpha(this.state[event.target.name])) { 
+                this.setState({
+                    [`${event.target.name}Error`]: "",
+                });
+            } else { 
+                this.setState({
+                    [`${event.target.name}Error`]: `${event.target.placeholder} can only have alphabet`,
+                    isButtonDisabled: true,
+                });
+            }
+        } else {
+            this.setState({ 
+                [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+                isButtonDisabled: true,
+            });
+        }
+    };
+
+    handleEmailInput = () => {
+        if (this.state.email.length === 0) { 
+            this.setState({
+                emailError: "Email cannot be empty",
+                isButtonDisabled: true,
+            });
+        } else {
+            if (isEmail(this.state.email)) { 
+                this.setState({
+                    emailError: "",
+                });
+            } else {
+                this.setState({
+                    emailError: "Please, enter a valid email!",
+                    isButtonDisabled: true,
+                });
+            }
+        }
+    };
+    
+    handlePasswordInput = () => {
+        if (this.state.confirmPasswordOnFocus) { 
+            if (this.state.password !== this.state.confirmPassword) {  
+                this.setState({
+                    confirmPasswordError: "Password does not match", 
+                    isButtonDisabled: true,
+                });
+            } else {
+                this.setState({
+                    confirmPasswordError: "",
+                });
+            }
+        }
+
+        if (this.state.password.length === 0) { 
+            this.setState({
+                passwordError: "Password cannot be empty", 
+                isButtonDisabled: true,
+            });
+        } else {
+            if (isStrongPassword(this.state.password)) { 
+                this.setState({
+                    passwordError: "", 
+                });
+            } else {
+                this.setState({ 
+                    passwordError:
+                        "Password must contains 1 uppercase, 1 lowercase, 1 special character, 1 number and minimul of 8 charactors long",
+                    isButtonDisabled: true,
+                });
+            }
+        }
+    };
 
     render() {
         const {
             firstName,
             lastName,
-            username,
             email,
             password,
-            confirmPassword,
+            firstNameError,
+            lastNameError,
+            emailError,
+            passwordError,
         } = this.state
 
         return (
@@ -52,7 +164,9 @@ export class Signup extends Component {
                                 // onBlur={this.handleOnBlur} // if we click here, and dont type, throws err in the firstNameError below.
                                 // onFocus={this.handleInputOnFocus} // if it is false, set to true
                                 />
-
+                                <div className="errorMessage">
+                                    {firstNameError && firstNameError} 
+                                </div>
                             </div>
                         </div>
 
@@ -69,6 +183,9 @@ export class Signup extends Component {
                                 // onBlur={this.handleOnBlur}
                                 // onFocus={this.handleInputOnFocus}
                                 />
+                                <div className="errorMessage">
+                                    {lastNameError && lastNameError}
+                                </div>
                             </div>
                         </div>
 
@@ -85,6 +202,9 @@ export class Signup extends Component {
                                 // onBlur={this.handleOnBlur}
                                 // onFocus={this.handleInputOnFocus}
                                 />
+                                <div className="errorMessage">
+                                    {emailError && emailError}
+                                </div>
                             </div>
                         </div>
 
@@ -101,9 +221,9 @@ export class Signup extends Component {
                                 // onBlur={this.handleOnBlur}
                                 // onFocus={this.handleInputOnFocus}
                                 />
-                                {/* <div className="errorMessage">
+                                <div className="errorMessage">
                                     {passwordError && passwordError}
-                                </div> */}
+                                </div>
                             </div>
                         </div>
 
@@ -115,13 +235,13 @@ export class Signup extends Component {
 
                         <div className='copyright'>
                             <p>
-                                By clicking "Continue", I agree to the <Link className='termsofservice'>Terms of Service</Link>  and <Link className='privacypolicy'>Privacy Policy</Link>.
+                                By clicking "Continue", I agree to the <Link to={{ pathname: "/login" }} className='termsofservice'>Terms of Service</Link>  and <Link to={{ pathname: "/login" }} className='privacypolicy'>Privacy Policy</Link>.
                             </p>
                             <div className='question'>
                                 <p>
                                 Already have an account? 
                             </p>
-                                <Link className='signIn'>Sign In</Link>
+                                <Link to={{ pathname:"/login"}} className='signIn'>Sign In</Link>
                                 </div>
                         </div>
                     </form>
---
+++ SKO-frontend/sko-frontend/src/utils/checkIfAuth.js
@@ -0,0 +1,22 @@
+import jwtDecode from "jwt-decode";
+import setAxiosAuthToken from "./setAxiosAuthToken";
+
+const checkIfAuth = () => {
+    let getJwtToken = window.localStorage.getItem('jwtToken')
+
+    if (getJwtToken){
+        const currentTime = Date.now()/1000;
+        let decodedToken = jwtDecode(getJwtToken);
+        if (decodedToken.exp<currentTime){
+            setAxiosAuthToken(null)
+            return false;
+        } else {
+            setAxiosAuthToken(getJwtToken)
+            return true;
+        }
+    } else {
+        return false
+    }
+}
+
+export default checkIfAuth
