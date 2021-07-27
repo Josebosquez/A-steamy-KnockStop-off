4eab9f4735386b6612026ec02f3904fb496aa557
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-07-27 09:47:11 -0600

    added toast.css to app.js, signup is complete

--- SKO-frontend/sko-frontend/src/App.js
+++ SKO-frontend/sko-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import MainRouter from "./MainRouter";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class App extends Component {
 state={
